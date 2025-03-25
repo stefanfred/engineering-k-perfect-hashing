071e67f10a44079ebb5c562be023f09015febb9d
ByteHamster <info@bytehamster.com>
2025-03-25 17:26:19 +0100

    Simplify constructor in PaCHash

--- include/pachash/PaCHash.hpp
+++ include/pachash/PaCHash.hpp
@@ -17,139 +17,130 @@ namespace kphf::PaCHash {
 
 #ifdef STATS
 struct Config {
-	int k;
-	double bucket_size;
+    int k;
+    double bucket_size;
 
-	bool operator<(const Config &rhs) const {
-		return k < rhs.k || (k == rhs.k && bucket_size < rhs.bucket_size);
-	}
+    bool operator<(const Config &rhs) const {
+        return k < rhs.k || (k == rhs.k && bucket_size < rhs.bucket_size);
+    }
 };
 
 struct Stat {
-	uint64_t ribbon_total = 0;
-	uint64_t keys_total = 0;
+    uint64_t ribbon_total = 0;
+    uint64_t keys_total = 0;
 };
 
 std::map<Config, Stat> stats;
 
 void dump_stats() {
-	for (auto [conf, stat]: stats) {
-		std::cout << "RESULT"
-			<< "\tk=" << conf.k
-			<< "\tbucket_size=" << conf.bucket_size
-			<< "\tribbon_per_key="
-				<< (double)stat.ribbon_total / stat.keys_total
-			<< "\n";
-	}
+    for (auto [conf, stat]: stats) {
+        std::cout << "RESULT"
+            << "\tk=" << conf.k
+            << "\tbucket_size=" << conf.bucket_size
+            << "\tribbon_per_key="
+                << (double)stat.ribbon_total / stat.keys_total
+            << "\n";
+    }
 }
 #endif
 
 class PaCHash {
 private:
-	uint64_t n_buckets;
-	EliasFano ef;
-	mutable SimpleRibbon<1> ribbon;
-
-	PaCHash(uint64_t n_buckets, EliasFano &&ef, SimpleRibbon<1> &&ribbon)
-		: n_buckets(n_buckets), ef(std::move(ef)), ribbon(std::move(ribbon)) {
-	}
-
+    uint64_t n_buckets;
+    EliasFano ef;
+    mutable SimpleRibbon<1> ribbon;
 public:
-	PaCHash() : n_buckets(0) {
-	}
+    uint64_t operator()(const std::string &item) const {
+        return operator()(Hash128(item));
+    }
 
-	PaCHash(int k, double bucket_size, const std::vector<Hash128> &keys):
-	  PaCHash(std::move(build(k, bucket_size, keys))) {}
+    uint64_t operator()(Hash128 key) const {
+        uint64_t bucket = bytehamster::util::fastrange64(key.hi, n_buckets);
+        auto [start, end] = ef.search(bucket+1);
+        int bits = std::bit_width(end - start);
+        uint64_t x = 0;
+        for (int j = 0; j < bits; j++) {
+            x |= uint64_t(ribbon.retrieve(key.lo + uint64_t(j))) << j;
+        }
+        return start + x - 1;
+    }
 
-	PaCHash(int k, double bucket_size, const std::vector<std::string> &keys):
-		PaCHash(std::move(build(k, bucket_size, keys))) {}
+    size_t count_bits() const {
+        return sizeof(*this) * 8
+            + ef.count_bits() - sizeof(ef) * 8
+            + ribbon.sizeBytes() * 8 - sizeof(ribbon) * 8
+        ;
+    }
 
-	uint64_t operator()(const std::string &item) const {
-		return operator()(Hash128(item));
-	}
+    PaCHash() : n_buckets(0) {
+    }
 
-	uint64_t operator()(Hash128 key) const {
-		uint64_t bucket = bytehamster::util::fastrange64(key.hi, n_buckets);
-		auto [start, end] = ef.search(bucket+1);
-		int bits = std::bit_width(end - start);
-		uint64_t x = 0;
-		for (int j = 0; j < bits; j++) {
-			x |= uint64_t(ribbon.retrieve(key.lo + uint64_t(j))) << j;
-		}
-		return start + x - 1;
-	}
-
-	size_t count_bits() const {
-		return sizeof(*this) * 8
-			+ ef.count_bits() - sizeof(ef) * 8
-			+ ribbon.sizeBytes() * 8 - sizeof(ribbon) * 8
-		;
-	}
+    PaCHash(int k, double bucket_size, const std::vector<std::string> &keys) {
+        n_buckets = std::ceil(keys.size() / bucket_size);
+        std::vector<Hash128> keysHashed;
+        keysHashed.reserve(keys.size());
+        for (const std::string & key : keys) {
+            Hash128 hash(key);
+            hash.hi = bytehamster::util::fastrange64(hash.hi, n_buckets);
+            keysHashed.push_back(hash);
+        }
+        build(k, bucket_size, keysHashed);
+    }
 
 private:
-	static PaCHash build(int k, double bucket_size, const std::vector<std::string> &keys) {
-		uint64_t n_buckets = std::ceil(keys.size() / bucket_size);
-		std::vector<Hash128> keysHashed;
-		keysHashed.reserve(keys.size());
-		for (const std::string & key : keys) {
-			Hash128 hash(key);
-			hash.hi = bytehamster::util::fastrange64(hash.hi, n_buckets);
-			keysHashed.push_back(hash);
-		}
-		return build(k, bucket_size, keysHashed);
-	}
+    void build(int k, double bucket_size, std::vector<Hash128> keys) {
+        uint64_t n_bins = (keys.size() + k - 1) / k;
+        n_buckets = std::ceil(keys.size() / bucket_size);
 
-	static PaCHash build(int k, double bucket_size, std::vector<Hash128> keys) {
-		uint64_t n_bins = (keys.size() + k - 1) / k;
-		uint64_t n_buckets = std::ceil(keys.size() / bucket_size);
+        ips2ra::sort(keys.begin(), keys.end(),[](const Hash128 &key) -> uint64_t { return key.hi; });
+        std::vector<uint64_t> bucket_sizes(n_buckets);
+        for (uint64_t i = 0; i < keys.size(); i++) {
+            bucket_sizes[keys[i].hi]++;
+        }
 
-		ips2ra::sort(keys.begin(), keys.end(),[](const Hash128 &key) -> uint64_t { return key.hi; });
-		std::vector<uint64_t> bucket_sizes(n_buckets);
-		for (uint64_t i = 0; i < keys.size(); i++) {
-			bucket_sizes[keys[i].hi]++;
-		}
+        std::vector<uint64_t> threshold(n_bins+1);
+        threshold.front() = 0;
+        for (size_t i = 1; i < n_bins; i++) {
+            uint64_t b = keys[k*i].hi;
+            if (keys[k*i-1].hi != b && bucket_sizes[b-1] < bucket_sizes[b]) {
+                b--;
+            }
+            threshold[i] = b + 1;
+        }
+        threshold.back() = n_buckets + 1;
 
-		std::vector<uint64_t> threshold(n_bins+1);
-		threshold.front() = 0;
-		for (size_t i = 1; i < n_bins; i++) {
-			uint64_t b = keys[k*i].hi;
-			if (keys[k*i-1].hi != b && bucket_sizes[b-1] < bucket_sizes[b]) {
-				b--;
-			}
-			threshold[i] = b + 1;
-		}
-		threshold.back() = n_buckets + 1;
+        std::vector<std::pair<uint64_t, uint8_t>> ribbon_data;
+        uint64_t offset = 0, next = 1;
+        for (uint64_t i = 0; i < n_buckets; i++) {
+            uint64_t start = next;
+            while (threshold[next] == i+1) {
+                next++;
+            }
+            uint64_t end = next;
 
-		std::vector<std::pair<uint64_t, uint8_t>> ribbon_data;
-		uint64_t offset = 0, next = 1;
-		for (uint64_t i = 0; i < n_buckets; i++) {
-			uint64_t start = next;
-			while (threshold[next] == i+1) next++;
-			uint64_t end = next;
-
-			int bits = std::bit_width(end - start);
-			uint64_t x = 0;
-			for (Hash128 key; (key = keys[offset]).hi == i; offset++) {
-				if (offset == (start+x) * k) x++;
-				for (int j = 0; j < bits; j++) {
-					ribbon_data.emplace_back(key.lo + uint64_t(j),
-					                         (x >> j) & 1);
-				}
-			}
-		}
+            int bits = std::bit_width(end - start);
+            uint64_t x = 0;
+            for (Hash128 key; (key = keys[offset]).hi == i; offset++) {
+                if (offset == (start+x) * k) {
+                    x++;
+                }
+                for (int j = 0; j < bits; j++) {
+                    ribbon_data.emplace_back(key.lo + uint64_t(j), (x >> j) & 1);
+                }
+            }
+        }
 
 #ifdef STATS
-		{
-			Stat &stat = stats[Config { k, bucket_size }];
-			stat.ribbon_total += ribbon_data.size();
-			stat.keys_total += keys.size();
-		}
+        {
+            Stat &stat = stats[Config { k, bucket_size }];
+            stat.ribbon_total += ribbon_data.size();
+            stat.keys_total += keys.size();
+        }
 #endif
 
-		return PaCHash(n_buckets,
-		  std::move(EliasFano(threshold)),
-		  std::move(SimpleRibbon<1>(ribbon_data)));
-	}
+        ribbon = SimpleRibbon<1>(ribbon_data);
+        ef = EliasFano(threshold);
+    }
 };
 
 }
