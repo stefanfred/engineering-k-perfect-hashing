38c64af2e466f2a061b7bad5232969406a55d7b5
ByteHamster <info@bytehamster.com>
2025-03-28 11:17:32 +0100

    Add evaluation script

---
+++ scripts/scatterplot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --array=0-5
+
+params="--numKeys 1M --numQueries 1M"
+jobsBase=(
+    "$params --chd"
+    "$params --hashDisplace"
+    "$params --pachash"
+    "$params --thresholdBased"
+    "$params --thresholdBasedConsensus"
+    "$params --kRecSplit"
+)
+jobsAll=()
+for i in "${!jobsBase[@]}"; do
+    jobsAll+=("${jobsBase[$i]} -k 10")
+    jobsAll+=("${jobsBase[$i]} -k 100")
+    jobsAll+=("${jobsBase[$i]} -k 1000")
+done
+
+if [ "$#" -ne 0 ]; then
+    # Use "--list" or "--jobs" or actually anything
+    # Print available jobs
+    for i in "${!jobsAll[@]}"; do
+        echo -e "$i \t ${jobsAll[$i]}"
+    done
+    exit 0
+fi
+
+echo "Host: $(hostname)"
+echo "Git commit: $(git rev-parse HEAD)"
+git status
+echo "Gcc version: $(gcc --version)"
+echo "-----"
+
+make Comparison
+strings Comparison | grep fPIC
+
+if [[ -z "$SLURM_ARRAY_TASK_COUNT" ]]; then
+    echo "This is a simple batch execution. Running all tasks."
+    for i in "${!jobsAll[@]}"; do
+        echo "./Comparison ${jobsAll[$i]}"
+        ./Comparison ${jobsAll[$i]}
+    done
+else
+    echo "This is a slurm array job. Running only the requested task."
+    sleep 5
+    echo "./Comparison ${jobsAll[$SLURM_ARRAY_TASK_ID]}"
+    ./Comparison ${jobsAll[$SLURM_ARRAY_TASK_ID]}
+fi
+
