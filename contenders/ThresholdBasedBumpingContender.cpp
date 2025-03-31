e790cbcf3d0de2aca45067fb74cf137ccf388712
Sebastian Kirmayer <ueibz@student.kit.edu>
2025-03-31 17:16:10 +0200

    threshold based bumping: overload depends on k

--- contenders/ThresholdBasedBumpingContender.cpp
+++ contenders/ThresholdBasedBumpingContender.cpp
@@ -3,11 +3,22 @@
 #include <gcem.hpp>
 #include "DispatchK.h"
 
+template <size_t k, size_t x, double overloadFrom, double overloadStep, int overloadStepCount>
+void dispatchOverload(size_t N) {
+	if constexpr (overloadFrom > 1.0) {
+		ThresholdBasedBumpingContender<k, overloadFrom, x>(N).run();
+	}
+	if constexpr (overloadStepCount > 0) {
+		dispatchOverload<k, x, overloadFrom + overloadStep, overloadStep, overloadStepCount - 1>(N);
+	}
+}
+
 
 template <size_t k, size_t xFrom, size_t xTo>
 void dispatchX(size_t N) {
     if constexpr (xFrom <= xTo) {
-        {ThresholdBasedBumpingContender<k, 1.30, xFrom>(N).run();}
+		constexpr double overload = 6/(gcem::sqrt(k) + gcem::log2(k));
+		dispatchOverload<k, xFrom, 1 + overload * 0.95, overload * 0.025, 4>(N);
     }
     if constexpr (xFrom <= xTo) {
         dispatchX<k, xFrom + 1, xTo>(N);
