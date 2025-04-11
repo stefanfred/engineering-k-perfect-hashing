4d413e8c51f4e5d02bcb490ffd232720a93917bf
ByteHamster <info@bytehamster.com>
2025-04-11 09:34:23 +0200

    Better selection of parameters for PaCHash

--- contenders/PaCHashContender.cpp
+++ contenders/PaCHashContender.cpp
@@ -4,7 +4,7 @@ void paCHashContenderRunner(size_t N, size_t k) {
     if (k == 0) {
         throw std::invalid_argument("k must be greater than 0");
     }
-    for (int i = 1; i < 40; i++) {
-        {PaCHashContender(N, k, i / 20.0).run();}
+    for (int a = 0.6 * k; a <= 2 * k; a += 0.2 * k) {
+        {PaCHashContender(N, k, a).run();}
     }
 }
