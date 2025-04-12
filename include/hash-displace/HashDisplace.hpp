9ec5dfb79e5bbd57315ffaa48bbd073696b34bee
Stefan Hermann <stefan.a.hermann@gmail.com>
2025-04-12 04:15:27 +0200

    faster remix, cached seeds for bucketing

--- include/hash-displace/HashDisplace.hpp
+++ include/hash-displace/HashDisplace.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <ranges>
+
 #ifdef STATS
 #include <map>
 #endif
@@ -15,43 +16,44 @@
 
 namespace kphf::HashDisplace {
 #ifdef STATS
-struct Config {
-    uint64_t n, k, bucket_size;
+    struct Config {
+        uint64_t n, k, bucket_size;
 
-    bool operator<(const Config &rhs) const {
-        return n != rhs.n ? n < rhs.n :
-               k != rhs.k ? k < rhs.k : bucket_size < rhs.bucket_size;
-    }
-};
+        bool operator<(const Config &rhs) const {
+            return n != rhs.n ? n < rhs.n :
+                   k != rhs.k ? k < rhs.k : bucket_size < rhs.bucket_size;
+        }
+    };
 
-template<typename BucketFunction>
-std::map<Config, std::pair<uint64_t, std::vector<uint64_t>>> stats;
+    template<typename BucketFunction>
+    std::map<Config, std::pair<uint64_t, std::vector<uint64_t>>> stats;
 
-template<typename BucketFunction>
-void dump_stats() {
-    for (const auto &[config, data]: stats<BucketFunction>) {
-        const auto &[count, totals] = data;
-        for (size_t i = 0; i < totals.size(); i++) {
-            std::cout << "RESULT"
-              << "\tbucket_function=" << BucketFunction::name()
-              << "\tn=" << config.n
-              << "\tk=" << config.k
-              << "\tbucket_size=" << config.bucket_size
-              << "\tbucket_idx=" << i
-              << "\tavg_seed=" << (double) totals[i]/count
-              << "\n";
+    template<typename BucketFunction>
+    void dump_stats() {
+        for (const auto &[config, data]: stats<BucketFunction>) {
+            const auto &[count, totals] = data;
+            for (size_t i = 0; i < totals.size(); i++) {
+                std::cout << "RESULT"
+                  << "\tbucket_function=" << BucketFunction::name()
+                  << "\tn=" << config.n
+                  << "\tk=" << config.k
+                  << "\tbucket_size=" << config.bucket_size
+                  << "\tbucket_idx=" << i
+                  << "\tavg_seed=" << (double) totals[i]/count
+                  << "\n";
+            }
         }
     }
-}
 #endif
 
-template<size_t k, typename BucketFunction, typename Encoding>
-class HashDisplace {
+    template<size_t k, typename BucketFunction, typename Encoding>
+    class HashDisplace {
     private:
         uint64_t nbuckets;
         uint64_t nbins;
         BucketFunction bucketFunction;
         Encoding seeds;
+        std::array<uint32_t, 10> fastSeeds;
     public:
         uint64_t operator()(const std::string &item) const {
             return operator()(Hash128(item));
@@ -59,23 +61,24 @@ class HashDisplace {
 
         uint64_t operator()(Hash128 item) const {
             uint64_t bucket = bucketFunction(item.hi);
-            uint64_t seed = seeds[bucket];
-            return bytehamster::util::fastrange64(bytehamster::util::remix(item.lo + seed), nbins);
+            uint64_t seed = bucket < fastSeeds.size() ? fastSeeds[bucket] : seeds[bucket];
+            return bytehamster::util::fastrange64(item.lo * ~seed, nbins);
         }
 
         [[nodiscard]] size_t count_bits() const {
             return 8 * sizeof(*this)
+                   + fastSeeds.size() * sizeof(uint32_t) * 8
                    + (seeds.count_bits() - 8 * sizeof(seeds));
         }
 
         HashDisplace()
-            : nbuckets(0), nbins(0), bucketFunction(0, 1.0) {
+                : nbuckets(0), nbins(0), bucketFunction(0, 1.0) {
         }
 
         HashDisplace(const std::vector<std::string> &items, uint64_t bucket_size, double load_factor = 1.0)
-            : nbuckets((items.size() + bucket_size - 1) / bucket_size),
-              nbins(ceil(items.size() / load_factor / k)),
-              bucketFunction(nbuckets, load_factor) {
+                : nbuckets((items.size() + bucket_size - 1) / bucket_size),
+                  nbins(ceil(items.size() / load_factor / k)),
+                  bucketFunction(nbuckets, load_factor) {
             assert(bucket_size > 0);
             assert(0.0 <= load_factor && load_factor <= 1.0);
 
@@ -85,12 +88,12 @@ class HashDisplace {
 
             std::vector<std::pair<uint64_t, uint64_t>> sorted_items;
             sorted_items.reserve(items.size());
-            for (const std::string & key : items) {
+            for (const std::string &key: items) {
                 Hash128 hash(key);
                 sorted_items.emplace_back(bucketFunction(hash.hi), hash.lo);
             }
             ips2ra::sort(sorted_items.begin(), sorted_items.end(),
-                [](const std::pair<uint64_t, uint64_t> &key) { return key.first; });
+                         [](const std::pair<uint64_t, uint64_t> &key) { return key.first; });
 
             using Iter = decltype(sorted_items)::iterator;
             using Range = std::ranges::subrange<Iter>;
@@ -116,18 +119,17 @@ class HashDisplace {
             std::vector<uint32_t> counts(nbins, 0);
             std::vector<uint64_t> seeds_vec(nbuckets);
 #ifdef STATS
-        auto &[stats_count, stats_vec] =
-          stats<BucketFunction>.try_emplace(Config { items.size(), k, bucket_size }, std::piecewise_construct, std::make_tuple(0), std::make_tuple(nbuckets)).first->second;
-        stats_count++;
-        auto stats_it = stats_vec.begin();
+            auto &[stats_count, stats_vec] =
+              stats<BucketFunction>.try_emplace(Config { items.size(), k, bucket_size }, std::piecewise_construct, std::make_tuple(0), std::make_tuple(nbuckets)).first->second;
+            stats_count++;
+            auto stats_it = stats_vec.begin();
 #endif
             for (auto [i, bucket]: buckets) {
                 uint64_t seed;
                 for (seed = 0;; seed++) {
                     uint64_t j;
                     for (j = 0; j < bucket.size(); j++) {
-                        uint64_t hash = bytehamster::util::fastrange64(
-                            bytehamster::util::remix(bucket[j].second + seed), nbins);
+                        uint64_t hash = bytehamster::util::fastrange64(bucket[j].second * ~seed, nbins);
                         if (counts[hash] == k) {
                             break;
                         }
@@ -137,17 +139,19 @@ class HashDisplace {
                         break;
                     }
                     while (j > 0) {
-                        uint64_t hash = bytehamster::util::fastrange64(
-                            bytehamster::util::remix(bucket[--j].second + seed), nbins);
+                        uint64_t hash = bytehamster::util::fastrange64(bucket[--j].second * ~seed, nbins);
                         counts[hash]--;
                     }
                 }
                 seeds_vec[i] = seed;
 #ifdef STATS
-            *stats_it++ += seed;
+                *stats_it++ += seed;
 #endif
             }
             seeds = Encoding(seeds_vec);
+            for (size_t i = 0; i < fastSeeds.size(); ++i) {
+                fastSeeds[i] = seeds_vec[i];
+            }
         }
-};
+    };
 }
